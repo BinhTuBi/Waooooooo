3935f24e2dfe82788c277a4a9fc17c12524e7470
BinhTuBi <buibinh8112006@gmail.com>
2025-12-16 09:40:46 +0700

    Add heartRate and steps fields to DashboardData

--- src/main/java/com/yourcompany/smartwatch/model/controller/DashboardData.java
+++ src/main/java/com/yourcompany/smartwatch/model/controller/DashboardData.java
@@ -1,5 +1,13 @@
 package com.yourcompany.smartwatch.model.controller;
 
+
+
+
+
+
+
+
+
 public class DashboardData {
     // ... (Dán toàn bộ nội dung file DashboardData.java đã cung cấp ở câu trả lời trước) ...
     private int heartRate;
